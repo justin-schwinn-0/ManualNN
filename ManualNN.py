6086f372d9e440e9d03a51abf8b2e86be3f61744
justin-schwinn-0 <justinsch1029@gmail.com>
2024-03-20 17:16:11 -0500

    added class def for activation functions

--- ManualNN.py
+++ ManualNN.py
@@ -1,1 +1,24 @@
-print("test")
+import pandas
+from ucimlrepo import fetch_ucirepo
+import numpy as np
+
+iris = fetch_ucirepo(id=53)
+
+
+class activationFunction:
+    def __init__(self, act, deriv) -> None:
+        self.activation = act
+        self.derivitive = deriv
+        pass
+
+def test1Act():
+    print("test1 act")
+
+def test1der():
+    print("test1 der")
+
+
+test1 = activationFunction(test1Act,test1der)
+
+test1.activation()
+test1.derivitive()
