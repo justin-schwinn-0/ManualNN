70f6ad49bfae00211173311310ba4ea174d6c8fe
justin-schwinn-0 <justinsch1029@gmail.com>
2024-03-20 17:37:14 -0500

    more stuff

--- ManualNN.py
+++ ManualNN.py
@@ -2,13 +2,17 @@ import pandas
 from ucimlrepo import fetch_ucirepo
 import numpy as np
 
-iris = fetch_ucirepo(id=53)
-
+dataset = fetch_ucirepo(id=294)
 
 class activationFunction:
     def __init__(self, act, deriv) -> None:
         self.activation = act
         self.derivitive = deriv
+        pass
+
+class perceptron:
+    def __init__(self) -> None:
+        
         pass
 
 def test1Act():
@@ -17,8 +21,19 @@ def test1Act():
 def test1der():
     print("test1 der")
 
+def makeNNforDataset(dataset):
+    print(dataset.metadata)
+    name = dataset.metadata["name"]
+    features = dataset.metadata["num_features"]
+    target = dataset.metadata["target_col"]
+    print("Making NN for Dataset: ", name)
+    print("using", features ,"input nodes")
 
 test1 = activationFunction(test1Act,test1der)
 
 test1.activation()
 test1.derivitive()
+
+print
+
+makeNNforDataset(dataset)
