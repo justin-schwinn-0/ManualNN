a393408ff6243c97c53d63717b2afb7c4c1f5d2a
justin-schwinn-0 <justinsch1029@gmail.com>
2024-03-21 19:25:46 -0500

    fianl by Alex, ty

--- ManualNN.py
+++ ManualNN.py
@@ -1,15 +1,51 @@
 import pandas
-from ucimlrepo import fetch_ucirepo
 import numpy as np
 import math
 
+from ucimlrepo import fetch_ucirepo
 powerplant = fetch_ucirepo(id=294)
+dataset = powerplant.data
+
+#The number to divide the features and targets by
+scaleFactor = 1000
 
 class activationFunction:
     def __init__(self, act, deriv) -> None:
         self.act = act
         self.der = deriv
         pass
+
+def sigmoid_act(net_sum):
+    sigmoid_act1 = 1 / (1 + math.exp(-net_sum)) 
+    return sigmoid_act1
+
+def sigmoid_der(sigmoid_act1):
+    # sigmoid_act1 = sigmoid_act(net_sum)
+    sigmoid_der1 = sigmoid_act1 * (1 - sigmoid_act1)
+    return sigmoid_der1
+
+def tanh_act(net_sum):
+    tanh_act1 = (math.exp(net_sum) - math.exp(-net_sum))/(math.exp(net_sum) + math.exp(-net_sum))
+    return tanh_act1
+
+def tanh_der(tanh_act1):
+    # tanh_act1 = tanh_act(net_sum)
+    tanh_der1 = 1 - tanh_act1 ** 2
+    return tanh_der1
+
+def relu_act(net_sum):
+    if net_sum > 0:
+        relu_act1 = net_sum
+    else:
+        relu_act1 = 0
+    return relu_act1
+
+def relu_der(neuronOutput):
+    if neuronOutput > 0:
+        relu_der1 = 1
+    else:
+        relu_der1 = 0
+    return relu_der1
 
 class Neuron:
     #Previous layer is a list of inputNeurons
@@ -60,7 +96,22 @@ def makeNNforDataset(dataset,activationFunction,LayersArray):
     return Nn
 
 def getDataRow(data,row):
-    return data.features.iloc[row].to_numpy()
+    return (data.features.iloc[row].to_numpy() / scaleFactor)
+    #return data.features.iloc[row].to_numpy()
+
+def getTargetData(data,row):
+    return (data.targets.iloc[row].to_numpy() / scaleFactor)
+    #return data.targets.iloc[row].to_numpy()
+
+def squarederror(dataset, network, numtraining, numtest):
+    a = 0
+    for elementIndex in range(numtraining, len(dataset.features)):
+        outputs = forwardPass(network,dataset,elementIndex)
+        #print(getTargetData(dataset, elementIndex) - outputs[-1][0])
+        a += ((getTargetData(dataset, elementIndex)*scaleFactor - outputs[-1][0]*scaleFactor))**2
+        #a += (getTargetData(dataset, elementIndex) - outputs[-1][0])**2
+
+    return (a / numtest)
 
 def forwardPass(Nn,data,row):
     dataRow = getDataRow(data,row)
@@ -79,11 +130,9 @@ def forwardPass(Nn,data,row):
             pass
 
     return NnOut
-
-def getTargetData(data,row):
-    return data.targets.iloc[row].to_numpy()
     
-def backwardsPass(Nn,data,targetRow,learningRate):
+def backwardsPass(Nn,data,targetRow,learningRate, allowance, momentum):
+    convergence = True
     inputs= getDataRow(data,targetRow)
     outputs = forwardPass(net,dataset,targetRow)
     target = getTargetData(data,targetRow)
@@ -106,87 +155,62 @@ def backwardsPass(Nn,data,targetRow,learningRate):
         pass
     pass
 
+    #If this is the first backward pass, need to fill out the momentum data structure
+    if len(momentum) == 0:
+        for layer in range(len(Nn)):
+            momentum.append([])
+            for neuron in range(len(Nn[layer])): 
+                momentum[layer].append([])
+                for weightIndex in range(1,len(Nn[layer][neuron].weights)):
+                    momentum[layer][neuron].append(0)
+
     for layer in range(len(Nn)):
         for neuron in range(len(Nn[layer])):
             Nn[layer][neuron].weights[0] += learningRate * delta[layer][neuron]
             for weightIndex in range(1,len(Nn[layer][neuron].weights)):
+                weightChange = 0
                 if layer == 0:
-                    Nn[layer][neuron].weights[weightIndex] += learningRate * delta[layer][neuron] * inputs[weightIndex-1]
+                    weightChange = learningRate * delta[layer][neuron] * inputs[weightIndex-1] + .9 * momentum[layer][neuron][weightIndex-1]
+                    Nn[layer][neuron].weights[weightIndex] += weightChange
                 else:
+                    weightChange = learningRate * delta[layer][neuron] * outputs[layer-1][weightIndex-1] + .9 * momentum[layer][neuron][weightIndex-1]
                     Nn[layer][neuron].weights[weightIndex] += learningRate * delta[layer][neuron] * outputs[layer-1][weightIndex-1]
+                momentum[layer][neuron][weightIndex-1] = weightChange
+
+                if weightChange > allowance:
+                    convergence = False
                 pass
             pass
         pass
     pass
 
-    return outputs[-1][-1],target
+    return outputs[-1][-1],target, convergence
 
 def epoch(Nn,data,begin,end,learningRate):
-    for i in range(begin,end):
-        out,target = backwardsPass(Nn,data,i,learningRate)
-        #print("Prediction:",out,"actual:",target, "delta:",(target-out))
+    converg = False
+    j = 1
+    momentum = []
+    while not converg:
+        print("Running epoch " + str(j))
+        #print(momentum)
+        j += 1
+        for i in range(begin,end):
+            out,target,converg = backwardsPass(Nn,data,i,learningRate, .0001, momentum)
+            #print("Prediction:",out,"actual:",target, "delta:",(target-out))
+        pass
     pass
 
-def squarederror(Nn,dataset, numtraining, numtest):
-    a = 0
-    for elementIndex in range(numtraining, len(dataset.features)):
-        outputs = forwardPass(Nn,dataset,elementIndex)
-        #print(getTargetData(dataset, elementIndex) - outputs[-1][0])
-        a += (getTargetData(dataset, elementIndex) - outputs[-1][0])**2
-
-    return (a / numtest)
-
-def sigmoid_act(net_sum):
-    sigmoid_act1 = 1 / (1 + math.exp(-net_sum)) 
-    return sigmoid_act1
-
-def sigmoid_der(sigmoid_act1):
-    # sigmoid_act1 = sigmoid_act(net_sum)
-    sigmoid_der1 = sigmoid_act1 * (1 - sigmoid_act1)
-    return sigmoid_der1
-
-def tanh_act(net_sum):
-    tanh_act1 = (math.exp(net_sum) - math.exp(-net_sum))/(math.exp(net_sum) + math.exp(-net_sum))
-    return tanh_act1
-
-def tanh_der(tanh_act1):
-    # tanh_act1 = tanh_act(net_sum)
-    tanh_der1 = 1 - tanh_act1 ** 2
-    return tanh_der1
-
-def relu_act(net_sum):
-    if net_sum > 0:
-        relu_act1 = net_sum
-    else:
-        relu_act1 = 0
-    return relu_act1
-
-def relu_der(neuronOutput):
-    if neuronOutput > 0:
-        relu_der1 = 1
-    else:
-        relu_der1 = 0
-    return relu_der1
 
 sigmoid = activationFunction(sigmoid_act,sigmoid_der)
 tanh = activationFunction(tanh_act,tanh_der)
 relu = activationFunction(relu_act,relu_der)
 
-dataset = powerplant.data
+
+net = makeNNforDataset(powerplant,relu,[5])
 
 
 numTraining = math.ceil(len(dataset.features) * .7)
 numTest = len(dataset.features) - numTraining
+epoch(net,dataset,0,numTraining, .01)
 
-
-scaledData = dataset.copy()
-print(dataset.features.iloc[0])
-print(dataset.targets.iloc[0])
-columns = ['AT','V','AP','RH']
-for col in columns:
-    scaledData["features"][col] = scaledData["features"][col] / scaledData["features"][col].abs().max()
-scaledData["targets"]["PE"] = scaledData["targets"]["PE"] / scaledData["targets"]["PE"].abs().max()
-
-net = makeNNforDataset(powerplant,relu,[5,5])
-epoch(net,dataset,0,numTraining,0.001)
-print("sqr Erorr:",squarederror(net,dataset, numTraining, numTest))
+print(squarederror(dataset, net, numTraining, numTest))
